88ea9b5404825646c5d628077e1f652b3416a4dd
MatthewRak <rak.matthew@gmail.com>
2018-03-12 19:36:46 -0500

    Initial Examples

---
+++ js/data.js
@@ -0,0 +1,44 @@
+
+var headerText = "Harvest Moon 64 Bingo";
+
+var footerText = " ";
+
+var winText = "Winner";
+
+var clickSnd = new Audio("audio/click.mp3");
+
+var winSnd = new Audio("audio/win.mp3");
+
+var JSONBingo = {"squares": [
+        {"square": "Marry Ann"},
+        {"square": "Marry Elli"},
+        {"square": "Marry Karen"},
+        {"square": "Marry Maria"},
+        {"square": "Marry Popuri"},
+        {"square": "Cliff and Ann Marry"},
+        {"square": "Jeff and Elli Marry"},
+        {"square": "Kai and Karen Marry"},
+        {"square": "Harris and Maria Marry"},
+        {"square": "Gray and Popuri Marry"},
+        {"square": "Gray grows flowers on Green Ranch"},
+        {"square": "Ocean Power Berry"},
+        {"square": "Farm Power Berry"},
+        {"square": "Mining Power Berry"},
+        {"square": "Restore the Vineyard"},
+        {"square": "Win the Horse Race"},
+        {"square": "Win the Dog Race"},
+        {"square": "Win the Swimming Festival"},
+        {"square": "Win the Cow Festival"},
+        {"square": "Win the Egg Festival"},
+        {"square": "Win the Vegetable Festival"},
+        {"square": "Become the Harvest King"},
+        {"square": "Obtain Ann's Photo"},
+        {"square": "Obtain Elli's Photo"},    	
+        {"square": "Obtain Karen's Photo"},    	
+        {"square": "Obtain Maria's Photo"},    	
+        {"square": "Obtain Popuri's Photo"}, 
+        {"square": "Build the Log Terrace"},    	
+        {"square": "Build the Greenhouse"},    	
+        {"square": "Find the Weather-vane in the Mine"}    	
+    ]
+};
